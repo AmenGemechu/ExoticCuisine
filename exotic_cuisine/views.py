759bcc1a82b9b02dd5f7f85b7e0a1eed4628caed
AmenGemechu <>
2023-09-22 13:15:36 +0000

    Bug fix on rendering templates, modify styles.css

--- exotic_cuisine/views.py
+++ exotic_cuisine/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 
 def get_base(request):
-    return render(request, 'exotic_cuisine/base.html')
+    return render(request, 'home.html')
 
 
-def get_home(request):
-    return render(request, 'exotic_cuisine/home.html')
