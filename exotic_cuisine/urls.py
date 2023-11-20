5d9fdfd2110560c4b5e2958f7b51fab2b1c3f727
AmenGemechu <>
2023-11-20 14:22:56 +0000

    Add open opst & view comment functionalities.

--- exotic_cuisine/urls.py
+++ exotic_cuisine/urls.py
@@ -8,13 +8,13 @@ app_name = 'exotic_cuisine'
 # are read top to bottom
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('register/', views.registerPage, name='register'),
     path('login_user/', views.login_user, name='login'),
     path('logout_user', views.logout_user, name='logout'),
     # path('', views.IndexView.as_view(), name='index'),
     # path('add/', views.AddView.as_view(), name='add'),
     # path('posts/', views.PostsView.as_view(), name='posts'),
-    # path('<slug:slug>/', views.SingleView.as_view(), name='single'),
     # path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
     path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
 ]
--- exotic_cuisine/views.py
+++ exotic_cuisine/views.py
@@ -1,13 +1,13 @@
-from django.contrib import messages
-from .forms import PostForm, CreateUserForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DeleteView, UpdateView, CreateView
-# from .model import exotic_cuisine
-from django.shortcuts import render  # redirect
-from django.views import generic
+from django.shortcuts import render, get_object_or_404  # redirect
+from django.views import generic, View
 from .models import Post
+from django.views.generic import ListView, DeleteView, UpdateView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import PostForm, CreateUserForm
+from django.contrib import messages
+# from .model import exotic_cuisine
 
 
 class PostList(generic.ListView):
@@ -15,6 +15,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6  # max_post per page
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
 
 
 # Posts
